cc3ab2cc1864d0d21ea0adf8a987bfe355b350e5
jragni <jagni001@ucr.edu>
2021-06-29 16:41:54 -0700

    FEAT: added submit functionality
    DESCRIPTION: allows submit button to make

--- app.js
+++ app.js
@@ -6,25 +6,41 @@ const $RESET_BUTTON = $("#reset-button");
 let $giphyForm = $("#giphy-form");
 let $searchText = $("#search-text")
 let $content = $("#main-content")
-let randomGifAPI = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=`
+let randomGifAPI = `https://api.giphy.com/v1/gifs/random?`
 
+
+// Submit Button Event Handler
 function submitHandler(evt){
 	evt.preventDefault();
-	// get value from search box
+	// get value from search box 
 	let searchQuery = $searchText.val();
-	console.log(evt);
-	console.log(searchQuery);
+
 	// add the search query to random gif API
 	// append the string to to API
-	let apiCall = randomGifAPI+searchQuery;
 	// make AJAX call to the API
-	// make async function for the call
-	// returns a promise which we will await for
-	//after finish awaiting put that object.
+	getRandomGif(randomGifAPI, searchQuery)
 	// make function to add the gif to the DOM
 }
 
-$SUBMIT_BUTTON.on("click", submitHandler)
+async function getRandomGif(url,searchQuery) {
+	let response = await axios.get(url, {params:{api_key:API_KEY, tag:searchQuery}});
+	console.log(response);
+	console.log('response.data',response.data)
+	console.log('response.data.data',response.data.data)
+
+  	addToGallery(response.data.data.image_original_url);
+}
+
+$SUBMIT_BUTTON.on("click", submitHandler);
+
+function addToGallery(gif) {
+	// Create JQUERY element
+	let $img = $('<img>')
+		.attr("src", gif)
+		.css({"height":"240px","width":"240px"})
+	
+	$content.append($img);
+}
 
 
 //API parameters q:query, api_key:api key
