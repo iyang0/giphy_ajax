03823074992a6e41036602322f1a1ebb0e06ae67
magus0 <harvestashya@gmail.com>
2021-06-29 15:59:22 -0700

    added form

---
+++ README.md
@@ -0,0 +1,20 @@
+Your application should do the following:
+
+1) Allow the user to search for a GIF and when the form is submitted, make an AJAX request to the Giphy API and return a single GIF
+
+2) Once the Giphy API has responded with data, append the GIF to the page
+
+3) Allow the user to search for as many GIFs as they would like and keep appending them to the page
+
+4) Allow the user to remove all of the GIFs by clicking a button
+
+
+### in HTML body have a form with a text input and two buttons
+	
+#### button to search
+	one of the buttons to call giphy API using axios with the text from the textbox as a parameter
+	
+#### after reciving  response from Giphy
+	After receiving the response, console.log the response data to make sure you’re getting back what you expect.
+
+###  
--- app.js
+++ app.js
@@ -1,1 +1,34 @@
+"use strict"
+
+const API_KEY="MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
+const $SUBMIT_BUTTON = $("#submit-button");
+const $RESET_BUTTON = $("#reset-button");
+let $giphyForm = $("#giphy-form");
+let $searchText = $("#search-text")
+let $content = $("#main-content")
+let randomGifAPI = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=`
+
+function submitHandler(evt){
+	evt.preventDefault();
+	// get value from search box
+	let searchQuery = $searchText.val();
+	console.log(evt);
+	console.log(searchQuery);
+	// add the search query to random gif API
+	// append the string to to API
+	let apiCall = randomGifAPI+searchQuery;
+	// make AJAX call to the API
+	// make async function for the call
+	// returns a promise which we will await for
+	//after finish awaiting put that object.
+	// make function to add the gif to the DOM
+}
+
+$SUBMIT_BUTTON.on("click", submitHandler)
+
+
+//API parameters q:query, api_key:api key
+//e.g. http://api.giphy.com/v1/gifs/search?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym
+
 console.log("Let's get this party started!");
+
